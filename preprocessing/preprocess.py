2bace26763159028be16edcb5851aefa3ca2fdd3
lubba-lubba-dub-dub <148734329+Lansuuuu@users.noreply.github.com>
2024-12-06 12:53:23 +0000

    modified:   preprocessing/preprocess.py

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -4,6 +4,6 @@ def preprocess_text(text):
     return text.lower().strip()
 
 def load_and_preprocess_data(filepath):
-    data = pd.read_csv(filepath)
+    data = pd.read_csv("/workspaces/support-ticket-classifier-chatbot/data/customer_support_tickets.csv")
     data['Ticket Message'] = data['Ticket Message'].apply(preprocess_text)
     return data
